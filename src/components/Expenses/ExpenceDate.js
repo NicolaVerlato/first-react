1f6bb5d022b2ccfe242fca9a2d517f4a3aad709f
NicolaVerlato <nicola.verlato00@gmail.com>
2023-06-18 08:57:23 +0200

    first commit

---
+++ src/components/Expenses/ExpenceDate.js
@@ -0,0 +1,16 @@
+import './ExpenceDate.css'
+
+function ExpenceDate(props) {
+    const month = props.date.toLocaleString('it-IT', { month: 'long' });
+    const day = props.date.toLocaleString('it-IT', { day: '2-digit' });
+    const year = props.date.getFullYear();
+    return(
+        <div className="expense-date">
+            <div className="expense-date__day">{day}</div>
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__year">{year}</div>
+        </div>
+    )
+}
+
+export default ExpenceDate;
